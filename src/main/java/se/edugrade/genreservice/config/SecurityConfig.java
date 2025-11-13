cdb26e48c0a5f88302e0f4e5d25941af625cbce4
Jocelyn <aracely.campos@hotmail.com>
2025-11-13 17:39:57 +0100

    Added Converter

--- src/main/java/se/edugrade/genreservice/config/SecurityConfig.java
+++ src/main/java/se/edugrade/genreservice/config/SecurityConfig.java
@@ -1,24 +1,29 @@
 package se.edugrade.genreservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.server.servlet.OAuth2AuthorizationServerJwtAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${edufy.security.client-id}")
+    private String clientId;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .headers(h-> h.frameOptions(f -> f.sameOrigin()))
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(HttpMethod.GET,"/edufy/v1/genre/test").permitAll()
@@ -29,34 +34,11 @@ public class SecurityConfig {
 
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .oauth2ResourceServer(auth2 -> auth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
     }
 
 
-    @Bean
-    public UserDetailsService userDetailsService() {
 
-        UserDetails user1 = User.withUsername("Erik")
-                .password("{noop}Edman")
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.withUsername("Jocelyn")
-                .password("{noop}CarrilloCampos")
-                .roles("USER")
-                .build();
-
-        UserDetails user3 = User.withUsername("Mohamed")
-                .password("{noop}Sharshar")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("Hugo")
-                .password("{noop}Ransvi")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2, user3, admin);
-    }
 }
